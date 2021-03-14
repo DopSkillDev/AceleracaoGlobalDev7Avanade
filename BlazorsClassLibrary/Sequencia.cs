7344ff50db6e2dfa52a26dc8f54452c6b8b4ec69
DaniloOPro <daniloopro@gmail.com>
2021-03-14 01:41:04 -0300

    projeto blazor aceleração global dev 7 dio

---
+++ BlazorsClassLibrary/Sequencia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorsClassLibrary
+{
+    public class Sequencia
+    {
+        public int[] GetLista(int[] numero)
+        {
+            int[] lista = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            numero = lista;
+
+            return numero;    
+        }
+
+        public string GetMensagem()
+        {
+            return "Está é um teste de instância com o Blazors";
+        }
+    }
+}
